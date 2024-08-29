e3fb11f75744e589054284f506027199154b26a3
Artiom Karimov <lunatic0259@gmail.com>
2024-08-29 23:07:14 +0300

    Update packages & configs

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
-import * as request from 'supertest';
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import supertest from 'supertest';
 import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,6 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  it('/ (GET)', async () =>
+    supertest(app.getHttpServer()).get('/').expect(200).expect('Hello World!'));
 });
